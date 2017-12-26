8157ce9c3ee5a8cd30da6c585bd5b3e4b6e71478
Arun Allamsetty <arun.allamsetty@gmail.com>
2017-12-26 11:29:45 -0700

    745. Update to include caching.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Solutions to LeetCode problems in Scala.
 
-745. [Prefix and Suffix Search](https://leetcode.com/problems/prefix-and-suffix-search/description/): [Source]() / [Test]()
+745. [Prefix and Suffix Search](https://leetcode.com/problems/prefix-and-suffix-search/description/):
+     [Source](https://github.com/aa8y/leetcode/blob/master/src/main/scala/co/aa8y/leetcode/PrefixAndSuffixSearch.scala) /
+     [Test](https://github.com/aa8y/leetcode/blob/master/src/test/scala/co/aa8y/leetcode/PrefixAndSuffixSearch.scala)
--- src/main/scala/co/aa8y/leetcode/PrefixAndSuffixSearch.scala
+++ src/main/scala/co/aa8y/leetcode/PrefixAndSuffixSearch.scala
@@ -1,17 +1,22 @@
 package co.aa8y.leetcode
 
-import scala.annotation.tailrec
-
 class WordFilter(words: Array[String]) {
+  val cache = {
+    val map = for {
+      (word, i) <- words.zipWithIndex
+      j <- 0 to word.size
+      k <- 0 to word.size
+    } yield {
+      val prefix = word.take(j)
+      val suffix = word.takeRight(k)
+      val key = (prefix, suffix)
+
+      (key -> i)
+    }
+    map.toMap
+  }
 
   def f(prefix: String, suffix: String): Int = {
-    @tailrec
-    def loop(i: Int, maxWeight: Int): Int = {
-      if (i == words.size) maxWeight
-      else if (words(i).startsWith(prefix) && words(i).endsWith(suffix) && i > maxWeight) {
-        loop(i + 1, i)
-      } else loop(i + 1, maxWeight)
-    }
-    loop(0, -1)
+    cache.get(prefix, suffix).getOrElse(-1)
   }
 }
