60bb487bb3f60a85c55456ae46471445a34f6155
Arun Allamsetty <arun.allamsetty@gmail.com>
2017-12-28 16:57:56 -0700

    745. Optimize for a List of sorted weights.

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 745. [Prefix and Suffix Search](https://leetcode.com/problems/prefix-and-suffix-search/description/):
      [Source](https://github.com/aa8y/leetcode/blob/master/src/main/scala/co/aa8y/leetcode/PrefixAndSuffixSearch.scala) /
-     [Test](https://github.com/aa8y/leetcode/blob/master/src/test/scala/co/aa8y/leetcode/PrefixAndSuffixSearch.scala)
+     [Test](https://github.com/aa8y/leetcode/blob/master/src/test/scala/co/aa8y/leetcode/PrefixAndSuffixSearchSpec.scala)
+     (Fails Test Case 10/12)
--- src/main/scala/co/aa8y/leetcode/PrefixAndSuffixSearch.scala
+++ src/main/scala/co/aa8y/leetcode/PrefixAndSuffixSearch.scala
@@ -1,5 +1,7 @@
 package co.aa8y.leetcode
 
+import scala.annotation.tailrec
+
 /**
  * Given many words, words[i] has weight i. Design a class WordFilter that supports one function,
  * WordFilter.f(String prefix, String suffix). It will return the word with given prefix and
@@ -41,9 +43,16 @@ class WordFilter(words: Array[String]) {
   }
 
   def f(prefix: String, suffix: String): Int = {
+    @tailrec
+    def loop(l: List[Int], r: List[Int]): Int = {
+      if (l.isEmpty || r.isEmpty) -1
+      else if (l.head > r.head) loop(l.tail, r)
+      else if (l.head < r.head) loop(l, r.tail)
+      else l.head
+    }
     prefixCache.get(prefix) match {
       case Some(pw) => suffixCache.get(suffix) match {
-        case Some(sw) => pw.intersect(sw).max
+        case Some(sw) => loop(pw, sw)
         case _ => -1
       }
       case _ => -1
