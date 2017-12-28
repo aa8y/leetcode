e73bfd10ae4d474dfa6be21ef0e2a653825ea59d
Arun Allamsetty <arun.allamsetty@gmail.com>
2017-12-28 16:46:30 -0700

    745. Separate caching for prefix and suffix.

--- src/main/scala/co/aa8y/leetcode/PrefixAndSuffixSearch.scala
+++ src/main/scala/co/aa8y/leetcode/PrefixAndSuffixSearch.scala
@@ -1,22 +1,52 @@
 package co.aa8y.leetcode
 
+/**
+ * Given many words, words[i] has weight i. Design a class WordFilter that supports one function,
+ * WordFilter.f(String prefix, String suffix). It will return the word with given prefix and
+ * suffix with maximum weight. If no word exists, return -1.
+ *
+ * Examples:
+ *   Input:
+ *     WordFilter(["apple"])
+ *     WordFilter.f("a", "e") // returns 0
+ *     WordFilter.f("b", "") // returns -1
+ *
+ * Note:
+ * 1. words has length in range [1, 15000].
+ * 2. For each test case, up to words.length queries WordFilter.f may be made.
+ * 3. words[i] has length in range [1, 10].
+ * 4. prefix, suffix have lengths in range [0, 10].
+ * 5. words[i] and prefix, suffix queries consist of lowercase letters only.
+ */
 class WordFilter(words: Array[String]) {
-  val cache = {
-    val map = for {
-      (word, i) <- words.zipWithIndex
-      j <- 0 to word.size
-      k <- 0 to word.size
-    } yield {
-      val prefix = word.take(j)
-      val suffix = word.takeRight(k)
-      val key = (prefix, suffix)
+  lazy val prefixCache = buildCache((s: String) => (i: Int) => s.take(i))
+  lazy val suffixCache = buildCache((s: String) => (i: Int) => s.takeRight(i))
 
-      (key -> i)
+  /**
+   * Takes as a parameter, a function takes in a string and an index and returns a substring.
+   */
+  private def buildCache(buildKey: String => Int => String) = {
+    words.zipWithIndex.foldLeft(Map[String, List[Int]]()) { (cache, next) =>
+      val (word, weight) = next
+
+      (0 to word.size).foldLeft(cache) { (localCache, i) =>
+        val k = buildKey(word)(i)
+        val w = localCache.get(k) match {
+          case Some(weights) => weight :: weights
+          case _ => weight :: Nil
+        }
+        localCache.updated(k, w)
+      }
     }
-    map.toMap
   }
 
   def f(prefix: String, suffix: String): Int = {
-    cache.get(prefix, suffix).getOrElse(-1)
+    prefixCache.get(prefix) match {
+      case Some(pw) => suffixCache.get(suffix) match {
+        case Some(sw) => pw.intersect(sw).max
+        case _ => -1
+      }
+      case _ => -1
+    }
   }
 }
