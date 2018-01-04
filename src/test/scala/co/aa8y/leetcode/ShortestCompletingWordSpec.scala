d1ab4e2f48526974b7b188cd736433a2ee94ca0b
Arun Allamsetty <arun.allamsetty@gmail.com>
2018-01-04 11:12:40 -0700

    Problem 62: Completed the tests.

--- src/test/scala/co/aa8y/leetcode/ShortestCompletingWordSpec.scala
+++ src/test/scala/co/aa8y/leetcode/ShortestCompletingWordSpec.scala
@@ -6,9 +6,45 @@ class ShortestCompletingWordSpec extends FunSpec {
   describe("Problem 749: Shortest Completing Word") {
     import ShortestCompletingWord._
 
-    describe("getLetters()") {
+    val emptyMap = Map[Char, Int]()
+    val foo = Map('f' -> 1, 'o' -> 2)
+    val foobar = Map('f' -> 1, 'o' -> 2, 'b' -> 1, 'a' -> 1, 'r' -> 1)
+
+    describe("getLetterFreq()") {
+      it("should return an empty map for an empty string.") {
+        assert(getLetterFreq("") === emptyMap)
+      }
+      it("should return an empty map for a string with no letters.") {
+        assert(getLetterFreq("900123") === emptyMap)
+      }
+      it("should return a map with letter frequencies.") {
+        val computed = getLetterFreq("foobar")
+        assert(computed === foobar)
+      }
+      it("should ignore non-letter characters.") {
+        val expected = Map('l' -> 1, 't' -> 1, 'c' -> 1, 'd' -> 1)
+        val computed = getLetterFreq("l33tc0d3")
+
+        assert(computed === expected)
+      }
+      it("should convert letters to lowercase.") {
+        val computed = getLetterFreq("fOoBaR")
+        assert(computed === foobar)
+      }
     }
-    describe("shortestCompletingWord()") {
+    describe("isComplete()") {
+      it("should return true for empty maps.") {
+        assert(isComplete(emptyMap, emptyMap))
+      }
+      it("should return true for the same maps.") {
+        assert(isComplete(foobar, foobar))
+      }
+      it("should return true when the word completes the license.") {
+        assert(isComplete(foo, foobar))
+      }
+      it("should return false when the word does not complete the license.") {
+        assert(!isComplete(foobar, foo))
+      }
     }
     describe("LeetCode") {
       it("Example #1") {
